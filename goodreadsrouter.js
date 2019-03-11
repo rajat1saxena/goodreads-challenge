cb153df23429d41fa7e29735506e940486c6d7ac
Rajat Saxena <rajat@raynstudios.in>
2019-03-11 19:05:54 +0530

    router path added for fetching book details

--- goodreadsrouter.js
+++ goodreadsrouter.js
@@ -42,7 +42,36 @@ router.get('/search/:text', (req, res) => {
 })
 
 router.get('/details/:id', (req, res) => {
-  res.status(200).send(JSON.stringify({ message: req.params.id }))
+  // construct a query string to be sent to GoodReads.
+  const qString = querystring.stringify({
+    key: constants.GR_API_KEY,
+    id: req.params.id
+  })
+  const goodreadsUrl = `${constants.GR_BOOK_DETAILS_URL}?${qString}`
+  request(goodreadsUrl, (err, response, body) => {
+    if (err) {
+      res
+        .status(500)
+        .send(JSON.stringify({
+          error: err.message
+        }))
+    }
+
+    if (response.statusCode === 200) {
+      res
+        .status(200)
+        .send(JSON.stringify({
+          data: body
+        }))
+    } else {
+      console.log(body)
+      res
+        .status(Number(response.statusCode))
+        .send(JSON.stringify({
+          error: 'Error encountered'
+        }))
+    }
+  })
 })
 
 module.exports = router
