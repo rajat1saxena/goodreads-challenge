59372e8e9b397d73de30ae0963666f3a99dfde5c
Rajat Saxena <raynstudios.rajat@gmail.com>
2019-03-11 19:15:16 +0000

    Trimmed commented out parts

--- constants.js
+++ constants.js
@@ -2,13 +2,6 @@
  * This file provides constant values used across the app.
  */
 
-// export const GR_API_KEY = 'dfvFMgil1TXpW5QaYgAaQ'
-// export const GR_SEARCH_URL = 'https://www.goodreads.com/search/index.xml'
-// export const GR_BOOK_DETAILS_URL = 'https://www.goodreads.com/book/show.xml'
-
-// // The following helps in dodging CORS and CORB issues
-// export const CORS_ENABLE_URL = 'https://cors-anywhere.herokuapp.com/'
-
 module.exports = {
   GR_API_KEY: 'dfvFMgil1TXpW5QaYgAaQ',
   GR_SEARCH_URL: 'https://www.goodreads.com/search/index.xml',
